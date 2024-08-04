bd5597886e96b6c77d3e8e4c5a7b853418ac2feb
Shashmitha Bandara <139967749+ShashmithaBan@users.noreply.github.com>
2024-08-04 12:17:24 -0700

    updated

--- server/modals/task.js
+++ server/modals/task.js
@@ -10,6 +10,10 @@ const taskSchema = new Schema({
     completed : {
         type : Boolean,
         default : false
+    },
+    owner : {
+        type: mongoose.Schema.Types.ObjectId,
+        required:true
     }
     
 })
